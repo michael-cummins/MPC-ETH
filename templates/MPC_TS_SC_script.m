1b2d8e0469854a1b052842887f312e234a324994
fglaurent <felix.laurent@yahoo.fr>
2023-05-19 18:09:03 +0200

    ...

--- templates/MPC_TS_SC_script.m
+++ templates/MPC_TS_SC_script.m
@@ -1,42 +1,43 @@
-close all;
-clear;
-clc;
+function MPC_TS_SC_script()
 
-params = generate_params();
-x0 = params.model.InitialConditionA;
-
-U_SC = zeros(params.model.nu, params.model.HorizonLength);
-U = ones(params.model.nu, params.model.HorizonLength);
-
-A = params.model.A;
-B = params.model.B; 
-Q = diag([94; 0.1579; 300; 0.01; 0.1; 0.1]);
-R = eye(params.model.nu);
-N = params.model.HorizonLength;
-[H, h] = lqr_maxPI(Q,R, params);
-mpc_TS = MPC_TS(Q,R,N,H,h,params);
-[ne,~] = size(params.model.StateRHS);
-
-x = x0;
-S = diag(1000*ones(ne));
-v = 1000;
-mpc_TS_SC = MPC_TS_SC(Q,R,N,H,h,S,v,params);
-
-same = true;
-for i = 1:params.model.HorizonLength
-    [U(:,i), ctrl_info] = mpc_TS.eval(x);
-    [U_SC(:,i), ctrl_SC_info] = mpc_TS_SC.eval(x);
-    if ctrl_info.feasible
-        if U(:,i) ~= U_SC(:,i)
-            same = false;
+    params = generate_params();
+    x0 = params.model.InitialConditionC;
+    
+    U_SC = zeros(params.model.nu, params.model.HorizonLength);
+    U = ones(params.model.nu, params.model.HorizonLength);
+    
+    A = params.model.A;
+    B = params.model.B; 
+    Q = diag([94, 0.1579, 300, 0.01, 0.1, 0.1]);
+    R = eye(params.model.nu);
+    N = 30;
+    [H, h] = lqr_maxPI(Q,R, params);
+    mpc_TS = MPC_TS(Q,R,N,H,h,params);
+    [ne,~] = size(params.constraints.StateRHS);
+    
+    x = x0;
+    S =10 *eye(ne);
+    v =100;
+    mpc_TS_SC = MPC_TS_SC(Q,R,N,H,h,S,v,params);
+    
+   
+    for i = 1:params.model.HorizonLength
+        i
+        [U(:,i), ctrl_info] = mpc_TS.eval(x);
+        [U_SC(:,i), ~] = mpc_TS_SC.eval(x);
+        if ctrl_info.ctrl_feas
+            
+            if U(:,i) - U_SC(:,i) > 1.0e-07
+                r = 'cest pas les memes'
+                break
+            end
+        
+        else 
+            r = 'unfeasible'
             break
         end
+        x = A*x + B*U(:,i);
     end
-    if ctrl_info.feasible == false
-        same = false;
-        break
-    end
-    x = A*x + B*U(:,i);
+    clear
+    save MPC_TS_SC_params.mat S v 
 end
-
-same
--- templates/compute_tightening.m
+++ templates/compute_tightening.m
@@ -8,4 +8,15 @@
 
 function params = compute_tightening(K_tube,H_tube,h_tube,params)  
 	% YOUR CODE HERE
+    epsilon = Polyhedron('A', H_tube, 'b', h_tube);
+    X = Polyhedron('A', params.constraints.StateMatrix, 'b', params.constraints.StateRHS);
+    U = Polyhedron('A', params.constraints.InputMatrix, 'b', params.constraints.InputRHS);
+
+    X_new = X.minus(epsilon);
+    U_new = U.minus(K_tube*epsilon);
+
+    params.constraints.StateMatrix = X_new.A;
+    params.constraints.StateRHS = X_new.b;
+    params.constraints.InputMatrix = U_new.A;
+    params.constraints.InputRHS = U_new.b;
 end
